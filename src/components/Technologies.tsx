daf38806ad9ef94c04a4891baa4fc548fd3b702f
Rifqi Setiawan <rifqis232@gmail.com>
2024-10-23 07:24:09 +0700

    final commit

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -21,16 +21,20 @@ const iconVariants = (duration: number) => ({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <motion.h1 
-      whileInView={{opacity: 1, y: 0}}
-      initial={{opacity: 0, y: -100}}
-      transition={{duration: 1.5}}
-      className="my-20 text-center text-4xl">Technologies</motion.h1>
-      <motion.div 
-      whileInView={{opacity: 1, x: 0}}
-      initial={{opacity: 0, x: -100}}
-      transition={{duration: 1.5}}
-      className="flex flex-wrap items-center justify-center gap-4">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
         <motion.div
           variants={iconVariants(2.5)}
           initial="initial"
